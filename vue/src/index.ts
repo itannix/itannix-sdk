3840c52c2ae7fb446b670358216d24882285dd3e
Mikael Rousson <mikael.rousson@gmail.com>
2026-01-25 11:05:29 +0800

    Update client components and add Vue SDK/quickstart

---
+++ vue/src/index.ts
@@ -0,0 +1,11 @@
+export { VoiceClient } from './VoiceClient';
+export { useVoiceClient } from './useVoiceClient';
+export { default as VoiceAssistant } from './VoiceAssistant.vue';
+export type {
+  ConnectionStatus,
+  VoiceClientOptions,
+  FunctionCallEvent,
+  AssistantMessageEvent,
+  VoiceClientCallbacks
+} from './VoiceClient';
+export type { UseVoiceClientOptions, UseVoiceClientReturn } from './useVoiceClient';
